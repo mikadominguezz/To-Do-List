e2ae2c56d154a139b76edcc765115cc08af2b130
mikadominguezz <mikadominguezz2004@gmail.com>
2025-09-08 08:19:45 -0300

    corrección checkbox

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for(i = 0; i < myNodeList.length; i++)
+{
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode()
+}
+
 
 var list = document.querySelector('ul')
 list.addEventListener('click', function(event){
@@ -8,34 +16,41 @@ list.addEventListener('click', function(event){
 }, false);
 
 function newTask() {
-    var newLi = document.createElement("li")
+    var newLi = document.createElement("li");
     var inputValue = document.getElementById("myNewTask").value;
-    var checkbox = document.createElement('checkbox');
-    checkbox.addEventListener('change', function(){
-         // Code to execute when the checkbox state changes
+
+    // ✅ Crear checkbox correctamente
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    // Evento para detectar cuando cambia su estado
+    checkbox.addEventListener("change", function() {
         if (this.checked) {
-            console.log('Checkbox is checked');
-            // Perform actions when checked
+            console.log("Checkbox is checked");
+            // Puedes marcar la tarea como hecha
+            newLi.classList.add("checked");
         } else {
-            console.log('Checkbox is unchecked');
-            // Perform actions when unchecked
+            console.log("Checkbox is unchecked");
+            newLi.classList.remove("checked");
         }
-    })
-    var item =  document.createElement('div');
-    
+    });
+
+    var item = document.createElement("div");
+
+    // Agrego el checkbox primero
     item.appendChild(checkbox);
-    
+
+    // Luego el texto de la tarea
     var nextNode = document.createTextNode(inputValue);
     item.appendChild(nextNode);
+
     newLi.appendChild(item);
-    if (inputValue == '')
-    {
+
+    if (inputValue === "") {
         alert("Debes escribir algo..");
+    } else {
+        document.getElementById("myUl").appendChild(newLi);
     }
-    else
-    {
-        document.getElementById("myUl").appendChild(newLi)
-    }
-    
+
     document.getElementById("myNewTask").value = "";
 }
